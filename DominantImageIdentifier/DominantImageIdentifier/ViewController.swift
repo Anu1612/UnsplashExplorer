aa3adb43d0fea67b29aefa0911c75391b1d98339
Anurag <myanuragojha@gmail.com>
2019-04-28 16:47:09 +0530

    Initializing Xcode Project

---
+++ DominantImageIdentifier/DominantImageIdentifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DominantImageIdentifier
+//
+//  Created by sonalojha on 28/04/19.
+//  Copyright © 2019 anuragojha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
